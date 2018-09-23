f2689774bc36bde2971957f3f2ed10d0656ef381
J <appsbyjj@gmail.com>
2018-09-23 19:47:43 +0200

    fix check style warnigns

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+*@param someone name of the person
+*@retun greeting string
+*/
+  final public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
